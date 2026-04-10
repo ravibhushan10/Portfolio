36044402c4ac846aa8519ce30febde0deb5e93f2
Ravi Bhushan <ravibhushankumar87tp@gmail.com>
2026-04-11 00:50:49 +0530

    ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import './index.css'
-import Navbar   from './components/Navbar/Navbar'
+import Navbar from "./components/Navbar/Navbar";
 import Hero     from './components/Hero/Hero'
 import About    from './components/About/About'
 import Projects from './components/Projects/Projects'
